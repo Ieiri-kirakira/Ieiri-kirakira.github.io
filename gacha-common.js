3a9c97dbe34f5536e9aeb2c72e199badf48061eb
Ieiri-kirakira <eoa3100@gmail.com>
2025-12-19 17:27:11 +0900

    Add drawGacha function for gacha mechanics

--- gacha-common.js
+++ gacha-common.js
@@ -1,1 +1,11 @@
-
+<script>
+function drawGacha(gacha, inputs) {
+  const text = gacha.slots[Math.floor(Math.random() * gacha.slots.length)];
+  let result = text;
+  inputs.forEach((v, i) => {
+    result = result.replaceAll(`[input${i+1}]`, v || "");
+  });
+  gacha.count++;
+  return result;
+}
+</script>
