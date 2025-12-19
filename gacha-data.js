3d817ef085b0b3abd63c51e94e262f4a7e2863e1
Ieiri-kirakira <eoa3100@gmail.com>
2025-12-19 17:26:43 +0900

    Update gacha-data.js

--- gacha-data.js
+++ gacha-data.js
@@ -1,1 +1,17 @@
+<script>
+window.GACHA_DATA = {
+  gachas: [
+    {
+      id: "daily2",
+      title: "2人の日常萌え！ガチャ",
+      inputs: ["せめ", "うけ"],
+      slots: [
+        "甘いものが得意でない[input1]の仕事場に、[input2]の好きなスイーツが増えていた。",
+        "テレビ番組を見て二人の子供の話になった[input1]と[input2]。"
+      ],
+      count: 0
+    }
+  ]
+};
+</script>
 
