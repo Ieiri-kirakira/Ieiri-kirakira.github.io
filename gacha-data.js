a039dc1dd0423f33646fdad68250c1a0d435c7e9
Ieiri-kirakira <eoa3100@gmail.com>
2025-12-20 18:51:09 +0900

    Update gacha-data.js

--- gacha-data.js
+++ gacha-data.js
@@ -1,17 +1,21 @@
-<script>
-window.GACHA_DATA = {
-  gachas: [
-    {
-      id: "daily2",
-      title: "2人の日常萌え！ガチャ",
-      inputs: ["せめ", "うけ"],
-      slots: [
-        "甘いものが得意でない[input1]の仕事場に、[input2]の好きなスイーツが増えていた。",
-        "テレビ番組を見て二人の子供の話になった[input1]と[input2]。"
-      ],
-      count: 0
-    }
-  ]
-};
-</script>
-
+const GACHAS = [
+  {
+    id: "nichijo",
+    title: "2人の日常萌え！ガチャ",
+    description: "日常系のお題が出ます",
+    inputs: ["name1", "name2"],
+    slots: [
+      "甘いものが得意でない{name1}の仕事場に…{name2}。",
+      "テレビ番組を見て子供の話になった{name1}と{name2}。",
+    ]
+  },
+  {
+    id: "breakup",
+    title: "別れた後のやつ",
+    description: "しんどめ",
+    inputs: ["name1"],
+    slots: [
+      "別れた後、一人でテレビを見る{name1}。",
+    ]
+  }
+];
